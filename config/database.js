393a27137ae2993dd5dd5319df22e6e05534567f
preetish-density <preetish.intern@density.exchange>
2023-01-25 10:19:48 +0530

    env files

--- config/database.js
+++ config/database.js
@@ -1,11 +1,17 @@
-const path = require('path');
+// path: ./config/env/production/database.js
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+        client: 'mysql',
+        connection: {
+          client: 'mysql',
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          username: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: env.bool('DATABASE_SSL'),
+        },
+        debug: false,
+      },
+  });
--- config/env/production/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-    host: env("HOST", "0.0.0.0"),
-    port: env.int("PORT", 1337),
-    url: "https://maritime-reflection.azurewebsites.net",
-    admin: {
-      url: "https://zealous-dune-072c96c10.2.azurestaticapps.net",
-      serveAdminPanel: false,
-    },
-  });
