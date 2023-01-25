f26dd3ca6123af7b9e0c6209de1a94d4665ec084
preetish-density <preetish.intern@density.exchange>
2023-01-25 10:21:49 +0530

    env files

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: "T9XNUPssbfwITT7Lgq0UEA==",
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: "ykqCrapP62n+AyOo7CAyhQ==",
   },
 });
